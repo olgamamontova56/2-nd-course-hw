333a78f931dc7beb5fad6f5e324209b13e311c27
John Doe <mamontova_olga56@mail.ru>
2023-10-13 15:03:52 +0300

    2 задание

--- javaScript/2.4.js
+++ javaScript/2.4.js
@@ -1,7 +1,14 @@
-//Задание 1
+// //Задание 1
 
-let n = 0;
-while ( n < 2) {
-    console.log('Привет');
-    n++
-}
+// let n = 0;
+// while ( n < 2) {
+//     console.log('Привет');
+//     n++
+// }
+
+//Задание 2
+ let n=1; 
+ while ( n <= 5 ) {
+    console.log(n);
+    n++;
+ }
